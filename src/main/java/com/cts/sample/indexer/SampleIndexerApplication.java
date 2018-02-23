15ceb1954b5f38965d850a7cb74e12a6836ccded
Prabaharan Rajendran <prabaharan.rajendran2@cognizant.com>
2018-02-23 17:02:14 +0530

    Initial project files

---
+++ src/main/java/com/cts/sample/indexer/SampleIndexerApplication.java
@@ -0,0 +1,12 @@
+package com.cts.sample.indexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleIndexerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cts/sample/indexer/service/IndexService.java
@@ -0,0 +1,8 @@
+package com.cts.sample.indexer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndexService {
+
+}
