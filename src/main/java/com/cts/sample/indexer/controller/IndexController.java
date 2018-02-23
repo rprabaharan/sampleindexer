a8eeb8062dadf368733a6d112883ccfd68ee5b0f
Prabaharan Rajendran <prabaharan.rajendran2@cognizant.com>
2018-02-23 19:05:35 +0530

    Sample model added

--- src/main/java/com/cts/sample/indexer/controller/IndexController.java
+++ src/main/java/com/cts/sample/indexer/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.cts.sample.indexer.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 	
+	@RequestMapping("/")
+	public String home() {
+		
+		return "Welcome to Index Controller";
+	}
 }
---
+++ src/main/java/com/cts/sample/indexer/model/SearchModel.java
@@ -0,0 +1,5 @@
+package com.cts.sample.indexer.model;
+
+public class SearchModel {
+
+}
